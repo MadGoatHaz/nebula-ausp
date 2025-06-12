2bdf4673ebb1ac422412d8ce88317d763c88ce23
MadGoatHaz <ghazlett@gmail.com>
2025-06-12 11:58:54 -0600

    Updates

--- assets/leaderboard-Cc7kaonL.js
+++ assets/leaderboard-Cc7kaonL.js
@@ -1,4 +1,4 @@
-import"./style-DCXveqyH.js";async function r(){try{const e=await fetch("http://localhost:3000/leaderboard");if(!e.ok)throw new Error(`HTTP error! status: ${e.status}`);const a=await e.json(),o=document.querySelector("#leaderboard-table tbody");if(!o)return;a.forEach(t=>{const d=document.createElement("tr");d.innerHTML=`
+import"./style-DPrC_ai0.js";async function r(){try{const e=await fetch("http://localhost:3000/leaderboard");if(!e.ok)throw new Error(`HTTP error! status: ${e.status}`);const a=await e.json(),o=document.querySelector("#leaderboard-table tbody");if(!o)return;a.forEach(t=>{const d=document.createElement("tr");d.innerHTML=`
                 <td>${t.rank}</td>
                 <td>${t.name}</td>
                 <td>${t.score}</td>
